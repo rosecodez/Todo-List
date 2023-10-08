b74f1878199444f6a1d65965f1cede37956c3fd4
Rose <rosebeats09@gmail.com>
2023-10-08 15:47:11 -0400

    Create basic html skeleton and page style

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,33 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: "development",
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'src/assets/images/[name].[ext]'
   },
- module: {
-   rules: [
-     {
-       test: /\.css$/i,
-       use: ['style-loader', 'css-loader'],
-     },
-   ],
- },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              root: path.resolve(__dirname, 'app')
+            }
+          }
+        ]
+      }
+    ],
+  },
 };
