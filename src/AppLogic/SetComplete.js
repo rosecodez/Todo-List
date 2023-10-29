673f6e942dbdabf5dacda99f1266b2ea03ca0142
Rose <rosebeats09@gmail.com>
2023-10-29 14:37:12 -0400

    Add setcomplete text content for title

--- src/AppLogic/SetComplete.js
+++ src/AppLogic/SetComplete.js
@@ -8,17 +8,19 @@ export function setComplete() {
     setCompleteBtn.style.padding = "6px 8px";
 
     const task = document.querySelector("#task-" + count);
-    
     let setCompleteCount = 2;
     setCompleteBtn.addEventListener("click", function() {
+        const titleText = document.querySelector("#titleText-" + count);
         setCompleteCount++;
         if(setCompleteCount % 2 == 0) {
             console.log("verify count is: " + setCompleteCount);
             task.style.backgroundColor = "#e11d487a";
+            titleText.textContent = "⁍Title: " + title.value + "(incomplete)";
         }
         else {
             console.log("verify count is: " + setCompleteCount);
             task.style.backgroundColor = "#1de16d7a";
+            titleText.textContent = "⁍Title: " + title.value + "(complete)";
         }
     })
     return setCompleteBtn
